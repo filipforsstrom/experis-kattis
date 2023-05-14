339e363f619dfb199b32bd913ddb780371ca2f6c
filipforsstrom <filipforsstrom@gmail.com>
2023-05-14 18:32:52 +0200

    problemb

---
+++ ProblemB/Program copy.cs
@@ -0,0 +1,54 @@
+﻿// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+
+// public class Program
+// {
+//     public static void Main()
+//     {
+//         string numberOfBuses = Console.ReadLine();
+//         int n = int.Parse(numberOfBuses);
+
+//         string buses = Console.ReadLine();
+//         int[] numbers = buses.Split(' ').Select(int.Parse).ToArray();
+
+//         Array.Sort(numbers);
+
+//         FindConsecutiveValues(numbers);
+//     }
+
+//     public static void FindConsecutiveValues(int[] buses)
+//     {
+//         int start = buses[0];
+//         int end = buses[0];
+
+//         for (int i = 2; i < buses.Length; i++)
+//         {
+//             if (buses[i] - buses[i - 1] == 1 && buses[i] - buses[i - 2] == 2)
+//             {
+//                 // Three consecutive elements
+//                 end = buses[i];
+//             }
+//             else
+//             {
+
+//                 if (start != end)
+//                 {
+//                     Console.Write($"{start}-{end} ");
+//                 }
+//                 else
+//                 {
+//                     Console.Write($"{start} ");
+//                 }
+
+//                 // Start a new consecutive range
+//                 start = buses[i];
+//                 end = buses[i];
+//             }
+//         }
+//     }
+// }
+
+
+// // 6
+// // 180 141 174 143 142 175
--- ProblemB/Program.cs
+++ ProblemB/Program.cs
@@ -6,21 +6,66 @@ public class Program
 {
     public static void Main()
     {
-        string firstLine = Console.ReadLine();
-        int n = int.Parse(firstLine);
+        string numberOfBuses = Console.ReadLine();
+        // string numberOfBuses = "8";
+        int n = int.Parse(numberOfBuses);
 
-        string secondLine = Console.ReadLine();
-        int[] numbers = secondLine.Split(' ').Select(int.Parse).ToArray();
+        string buses = Console.ReadLine();
+        // string buses = "181 180 141 174 143 142 175 171 172";
+        int[] numbers = buses.Split(' ').Select(int.Parse).ToArray();
 
         Array.Sort(numbers);
 
-        Console.WriteLine("First Line: " + firstLine);
-        Console.WriteLine("Second Line: " + secondLine);
+        FindConsecutiveValues(numbers, n);
+    }
 
-        Console.WriteLine("Numbers:");
-        foreach (int number in numbers)
+    public static void FindConsecutiveValues(int[] buses, int n)
+    {
+        int start = buses[0];
+        int end = buses[0];
+
+        for (int i = 1; i < buses.Length; i++)
         {
-            Console.WriteLine(number);
+            if (buses[i] - buses[i - 1] == 1)
+            {
+                end = buses[i];
+            }
+            else
+            {
+                if (start != end && end - start != 1)
+                {
+                    Console.Write($"{start}-{end} ");
+                }
+                else if (start != end)
+                {
+                    Console.Write($"{start} {end} ");
+                }
+                else
+                {
+                    Console.Write($"{start} ");
+                }
+
+                start = buses[i];
+                end = buses[i];
+            }
+        }
+
+        // final iteration
+        if (start != end && end - start != 1)
+        {
+            Console.Write($"{start}-{end} ");
+        }
+        else if (start != end)
+        {
+            Console.Write($"{start} {end} ");
+        }
+        else
+        {
+            Console.Write($"{start} ");
         }
     }
 }
+
+
+// 6
+// 180 141 174 143 142 175
